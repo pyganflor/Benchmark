20a646a8d8e61b58be13e7591dd703aa0a9905ac
Obrian Villasmil <obrian@pyganflor.com>
2020-04-13 15:45:44 -0500

    updates

--- resources/views/planta_variedad/inicio.blade.php
+++ resources/views/planta_variedad/inicio.blade.php
@@ -9,7 +9,7 @@
         <!-- Rounded tabs -->
         <ul id="myTab" role="tablist"
             class="nav nav-tabs nav-pills flex-column flex-sm-row text-center bg-light border-0 rounded-nav">
-            @if(session('id_rol') === 3)
+            @if(session('id_rol') === 3 || session('id_rol') ===1)
                 <li class="nav-item flex-sm-fill">
                     <a id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home"
                        aria-selected="true" class="nav-link border-0 text-uppercase font-weight-bold active">
@@ -23,15 +23,17 @@
                     </a>
                 </li>
             @endif
-            <li class="nav-item flex-sm-fill">
-                <a id="accesos-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
-                   aria-selected="false" class="nav-link border-0 text-uppercase font-weight-bold {{session('id_rol') != 3 ? 'active' : '' }}">
-                    Asignar variedades
-                </a>
-            </li>
+            @if(session('id_rol') === 2)
+                <li class="nav-item flex-sm-fill">
+                    <a id="accesos-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile"
+                       aria-selected="false" class="nav-link border-0 text-uppercase font-weight-bold {{session('id_rol') != 3 ? 'active' : '' }}">
+                        Asignar variedades
+                    </a>
+                </li>
+            @endif
         </ul>
         <div id="myTabContent" class="tab-content">
-            @if(session('id_rol') === 3)
+            @if(session('id_rol') === 3 || session('id_rol') ===1)
                 <div id="home" role="tabpanel" aria-labelledby="home-tab" class="tab-pane fade px-4 py-5 show active">
                     <div class="row" id="tabla_plantas"></div>
                 </div>
@@ -39,9 +41,11 @@
                     <div class="row" id="tabla_variedades"></div>
                 </div>
             @endif
-            <div id="profile" role="tabpanel" aria-labelledby="accesos-tab" class="tab-pane fade px-4 py-5 {{session('id_rol') != 3 ? 'show active' : '' }}">
-                <div class="row" id="tabla_asigna_variedad"> </div>
-            </div>
+            @if(session('id_rol') === 2)
+                <div id="profile" role="tabpanel" aria-labelledby="accesos-tab" class="tab-pane fade px-4 py-5 {{session('id_rol') != 3 ? 'show active' : '' }}">
+                    <div class="row" id="tabla_asigna_variedad"> </div>
+                </div>
+            @endif
         </div>
         <!-- End rounded tabs -->
     </div>
