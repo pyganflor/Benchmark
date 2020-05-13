425bf7bc4460a769268f0a618aebaab4ae078053
Obrian villasmil <obrian@pyganflor.com>
2020-05-13 17:10:49 +0000

    updates

--- resources/views/mails/accesos.blade.php
+++ resources/views/mails/accesos.blade.php
@@ -1,4 +1,13 @@
+<div style="text-align:center">
+    <img style="width:20%" src="{{url('img/logo_yura_brenchmark.png')}}">
+</div>
+<div style="width:80%;margin:50px auto;text-align:justify;margin-top:55px;">
+    <p><b>Hola</b></p>
+    <p>Queremos darte la bienvenida al servicio de Yura Benchmark. para acceder, recuerda tus datos de ingreso:</p>
+    <p style="margin-top:40px;text-align:center">
+        <b>Usuario : {{$user}}</b>
+        <br/>
+        <b>Contrasena : {{$pass}}</b>
+    </p>
+</div>
 
- Usuario : {{$user}}
-<br/>
- Contrasena : {{$pass}}
